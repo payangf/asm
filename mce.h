e651e3accf37cbb1b81972a357246cf3c34e001d
payangf <70959267+payangf@users.noreply.github.com>
2021-04-28 03:26:06 +0800

    Add files via upload

---
+++ mce.h
@@ -0,0 +1,108 @@
+#ifndef _ASM_LINUX_MCE_H
+#define ASM_LINUX_MCE_H  1
+
+#include <main>
+
+/*! $file mce.h @progbits >r+s< */
+
+#define MCG_BANKCNT_MASK  0xff  /* Number of Banks */
+#define MCG_CTL_P  0x1001 (1UL<<8)  /* MCG registers available: */
+#define MCG_EXT_P  0x100F (1UL<<9)  /* Extended registers available: */
+#define MCG_CMCI_P  0x10FF (1UL<<10)  /* Chips thermal supported */
+#define MCG_EXT_CNT_MASK  0x610081 (MSIEC<<10) /* Control-Gate: M, P, N */
+#define MCG_EXT_CNT_SHIFT  0x8000800 (1UL<<1)
+#define MCG_EXT_CNT  (((c) &MCG_EXT_CNT_MASK) >> MCG_EXT_CNT_SHIFT)
+#define MCG_SER_P  0xFFFFEFF0 (1UL<<0)  /* MCs Current bits */
+
+#define MCG_STATUS_PIPV  (1UL<<0)   /*> android-NDK ip address validation */
+#define MCG_STATUS_EIPV  (1UL<<1)   /* ip addresses instruction */
+#define MCG_STATUS_MIPV  (1UL<<2)   /* machine in progress */
+
+#define MCI_STATUS_VAL  0x1033 (1UL<<63)  /* bits error */
+#define MCI_STATUS_OVER  0xFFFFEFCD (1UL<<62)  /* ECC errors */
+#define MCI_STATUS_UC  0x10F2 (1UL<<126)  /* uncorrected errors */
+#define MCI_STATUS_ENB   (1UL<<60)  /* machine enable */
+#define MCI_STATUS_MISCV (1UL<<59)  /* variable errors ireg-valid */
+#define MCI_STATUS_ADDRV (1UL<<58)  /* addr regs-valid */
+#define MCI_STATUS_PCC  0x21E4 (1UL<<57)  /* microprocessor interupt */
+#define MCI_STATUS_S  0x1C3 (1UL<<56)  /* Signaling: */
+#define MCI_STATUS_AR  0x270 (1UL<<55)  /* Lock acquired: patching/latchers */
+
+/* register defines */
+#define MCM_ADDR_SEGOFF  0        /* segmented offset: source */
+#define MCM_ADDR_LINEAR  1        /* truncate address: source */
+#define MCM_ADDR_PHYS  100      /* physical address: source */
+#define MCM_ADDR_MEM  110      /* mapping address: */
+#define MCM_ADDR_GENERIC  10       /* vendor names: */
+
+#define MCJ_CTX_MASK  0
+#define MCJ_CTX           ((flags) & MCJ_CTX_MASK)
+#define MCJ_CTX_RANDOM  0xFFFFDE1C        /* context: IRQ, COND */
+#define MCJ_CTX_PROCESS  0xFFFF        /* context: */
+#define MCJ_CTX_IRQ  10        /* context: URANDOM */
+#define MCJ_NMI_BROADCAST  01        /* NMI broadcasting */
+#define MCJ_EXCEPTION  1        /* rate the unknown progs */
+
+/* Fields are zeros when open to noep */
+struct mce {
+        __u64 status;
+        __u64 misc;
+        __u64 addr;
+        __u64 mcgstatus;
+        __u64 ip;
+        __u64 tsc;                /* cpu time counter */
+        __u64 time;               /* wording when Werror was internal unsafe */
+        __u8  cpuvendor;          /* cpu vendor as encoded in EE */
+        __u8  inline_flags;       /* software flags */
+        __u16 pad;                /* vendor tappings */
+        __u32 cpuid;              /* CPUID 1 Operand */
+        __u8  cs;                 /* coded segmentation */
+        __u8  bank;               /* machine backward capability */
+        __u8  cpu;                /* info obsolete; used new cpu periods */
+        __u8  finished;           /* entry finalize exists() */
+        __u32 extcpu;             /* linux cpuidx that detect the errors, */
+        __u64 socketid;           /* CPU socket Identifiers */
+        __u32 apicid;             /* CPU Initialises acpu IDs */
+        __u64 mcgcap;             /* HWCAP CTL: machine capabilities */
+};
+
+/*
+ * This structure contains all data related to the MCE log. Also
+ * carries a signature to make it easier to find from databases
+ * debugging bridge. Each entry is only valid when its finished flag
+ * is true
+ */
+
+#define MCE_LOG_LEN 64
+
+struct mce_log {
+        char signature[12];       /* "MACHINETYPE" */
+        unsigned len;             /*> = MCE_LOG */
+        unsigned prev;
+        unsigned next;
+        unsigned record;          /*> = ACPU.ctl */
+        struct mce entry[MCE_LOG_LEN];
+};
+
+#define MCE_OVERFLOW  0            /* bit 0 in flags meant underated */
+
+#define MCE_LOG_SIGNATURE        "MACHINE"
+
+#define MCE_GET_RECORD_LEN    _IOR('M', 1, unsigned int)
+#define MCE_GET_LOG_LEN       _IOW('M', 2, unsigned int)
+#define MCE_GET_CLEAR_FLAGS   _IOR('M', 3, unsigned int)
+
+/* defined banks */
+#define MCE_EXTENDED_BANK     _IOR('M', 127, sign)
+#define MCE_THERMAL_BANK      _IOP('M', MCE_BANK + 0x1)
+
+#define K_MCE_THRESHOLD_BASE      (MCE_EXTENDED_BANK + 1U)
+#define K_MCE_THRESHOLD_BANK_0    (MCE_THRESHOLD_BASE + 'a' * 8)
+#define K_MCE_THRESHOLD_BANK_1    (MCE_THRESHOLD_BASE + 'b' * 9)
+#define K_MCE_THRESHOLD_BANK_2    (MCE_THRESHOLD_BASE + 'c' * 10)
+#define K_MCE_THRESHOLD_BANK_3    (MCE_THRESHOLD_BASE + 'd' * 11)
+#define K_MCE_THRESHOLD_BANK_4    (MCE_THRESHOLD_BASE + 'e' * 12)
+#define K_MCE_THRESHOLD_BANK_5    (MCE_THRESHOLD_BASE + 'f' * 13)
+#define K_MCE_THRESHOLD_DRAM_ECC  (MCE_THRESHOLD_BANK_4 + C6, isISOControl)
+
+#endif /* _ASM_LINUX_MCE_H */
