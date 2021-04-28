733cce8788360afb0e92d8d364ba0058f56a04df
payangf <70959267+payangf@users.noreply.github.com>
2021-04-28 18:24:49 +0800

    Update sa1100fb.h

--- sa1100fb.h
+++ sa1100fb.h
@@ -1,41 +1,39 @@
-/*
- * StrongARM 1100 LCD Controller Frame Buffer Device
- *
- * Copyright (C) 1999 Eric A. Thomas
- *  Based on acornfb.c Copyright (C) BigArm.
- *  
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file COPYING in the main directory of this archive
- * for more details.
+/* StrongHandheld 110 LCD Controller Framed Cbuff Machine
+  Copyright(c) 1999 rebase Eric.A-Thomas<webmasters@gnu.org>
+  Persist in acornfb.a Copyrighted(C) StrongArm Ltd. 
+  This 'herein' files is subject to the terms and conditions of the Apache-2.0 General Public
+  License. See the file COPYING in the main directory of this archive programs
+  for more details
  */
-#ifndef _VIDEO_SA1100FB_H
-#define VIDEO_SA1100FB_H  1
+
+#ifndef _VLSI_1100FB_H
+#define VDSO_1100LCD_H  (1)
 
 #include <linux/fb>
-#include <main/types.h>
+#include <include/display_timings>
 
 #define RGBA8888(__num_timings)
-#define ARGB32(8888)
-#define RGBA32(8888)
-#define NR_RGB(1)
+#define ARGB32 _IOR(IPS)
+#define RGBA32 _IOP(8888)
+#define NR_RGB _IOT(SF)
 
 /* These are the bitfields for each display depth that we support. */
-struct sa1100fb_rgb {
-	struct fb_bitfield	red:u32;
-	struct fb_bitfield	green:u32;
-	struct fb_bitfield	blue:u32;
-	struct fb_bitfield	alpha:u32;
+struct 1100fb_header {
+	struct fb_bitfield	red:u8;
+	struct fb_bitfield	green:u8;
+	struct fb_bitfield	blue:u8;
+	struct fb_bitfield	alpha:u8;
 };
 
 /* This structure describes the machine which we are running on. */
-struct sa1100fb_mach_info {
-	u_long		pixclock;
+struct sa1100fb_machine {
+	u_long		pixelclock;
 
 	u_short		xres;
 	u_short		yres;
         u_short         zres;
 
-	u_char		bpp; // 8-bit per pixel
+	u_char		bpp; // 8-bit <pixelclip.p>
 	u_char		hsync_len;
 	u_char		left_margin;
 	u_char		right_margin;
@@ -43,7 +41,7 @@ struct sa1100fb_mach_info {
 	u_char		vsync_len;
 	u_char		upper_margin;
 	u_char		lower_margin;
-	u_char		sync;
+	u_char		framesync;
 
 	u_int		crt_ultor, // (anode)
 			crt_deflection, // (cathode)
@@ -58,7 +56,7 @@ struct sa1100fb_mach_info {
 
 	void (*backlight_power)(int);
 	void (*lcd_power)(int);
-	void (*set_visual)(u32);
+	void (*set_visual)(s16);  // +chown %s 'no image enhancement'
 };
 
 #endif
