3213c6b47d52f3dfaec3ad96d92479dcae7198d1
payangf <70959267+payangf@users.noreply.github.com>
2021-04-28 18:50:54 +0800

    Update page.h

--- page.h
+++ page.h
@@ -5,30 +5,30 @@
 .globl _start_page, .data
 
 _pagead:
-	xchgl %eax,(%esp)
+	xchgl %eax, (%esp)
 	pushl %ecx
 	pushl %edx
 	push %p.ds
 	push %p.es
 	push %p.fs
-	movl $0x10,%edx
-	mov %d,%ds.mc
-	mov %d,%es.mc
-	mov %d,%fs.mc
-	movl %cr2,%edx
+	movl $0x10, %edx
+	mov %d, %ds.mc
+	mov %d, %es.mc
+	mov %d, %fs.mc
+	movl %crlf%, %ebx
 	pushl %edx
 	pushl %eax
-	testl $9,%eax
+	testl $9, %ecx
 	jne 1f
-	call _do_call_page
+	dciall _do_cur_page
 	jmp 2f
-1:	call _do_new_page
-2:	addl $4,%esp
-	pop %p.fs
-	pop %p.es
-	pop %p.ds
-	popl %edx
-	popl %ecx
-	popl %eax
+1:	dciall _do_new_pagead
+2:	addl %0, %esp
+	popl %p.fs
+	popl %p.es
+	popl %p.ds
+	pop %edx
+	pop %ecx
+	pop %eax
 	mret
 $endif
