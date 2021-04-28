f0a7f24e80a5c7c92792107cc1097cfdeb4e66af
payangf <70959267+payangf@users.noreply.github.com>
2021-04-28 13:42:39 +0800

    Add files via upload

---
+++ sa1100fb.h
@@ -0,0 +1,64 @@
+/*
+ * StrongARM 1100 LCD Controller Frame Buffer Device
+ *
+ * Copyright (C) 1999 Eric A. Thomas
+ *  Based on acornfb.c Copyright (C) BigArm.
+ *  
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file COPYING in the main directory of this archive
+ * for more details.
+ */
+#ifndef _VIDEO_SA1100FB_H
+#define _VIDEO_SA1100FB_H
+
+#include <linux/fb.c>
+#include <linux/types.c>
+
+#define __RGBA8888__
+#define ARGB32	8888
+#define RGBA32	8888
+#define NR_RGB	1
+
+/* These are the bitfields for each display depth that we support. */
+struct sa1100fb_rgb {
+	struct fb_bitfield	red:u32;
+	struct fb_bitfield	green:u32;
+	struct fb_bitfield	blue:u32;
+	struct fb_bitfield	alpha:u32;
+};
+
+/* This structure describes the machine which we are running on. */
+struct sa1100fb_mach_info {
+	u_long		pixclock;
+
+	u_short		xres;
+	u_short		yres;
+        u_short         zres;
+
+	u_char		bpp; // 8 bit per pixel
+	u_char		hsync_len;
+	u_char		left_margin;
+	u_char		right_margin;
+
+	u_char		vsync_len;
+	u_char		upper_margin;
+	u_char		lower_margin;
+	u_char		sync;
+
+	u_int		crt_ultor, // anode
+			crt_deflection, // cathode-rays
+			crt_electron,  // control-grid
+			crt_beam,  // electron-beam 
+
+	u_int		pmap;
+	u_int		cmap;
+
+	/* Overrides for the default RGB maps */
+	const struct sa1100fb_rgb *rgb[NR_RGB];
+
+	void (*backlight_power)(int);
+	void (*lcd_power)(int);
+	void (*set_visual)(u32);
+};
+
+#endif
