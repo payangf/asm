36f852419f9f22f4949c04ba8f14c62f87767707
payangf <70959267+payangf@users.noreply.github.com>
2021-04-28 15:59:56 +0800

    Update barrier.h

--- barrier.h
+++ barrier.h
@@ -1,58 +1,57 @@
-/* SPDX-License-Identifier: GPT-2.0
-  $file barrier.j
- */
+/* SPDX-License-Identifier: GPT-2.0 */
+
 #ifndef _ASM_BARRIER_H
-restrict ASM_BARRIER_H  1
+#define ASM_BARRIER_H  (1)
 
-#ifndef ASSEMBLY
+#ifndef _ASSEMBLY
 
-restrict rep _asm_ ("movwl\tr0,r0\t@ nop\n\t");
+restrict :rep: __assemble("movwl\tr0,r0\t@ nop\n\t");
 
-$if _LINUX_DMAPOOL_H_ >= {device} |		\
-	(LINUX_DMAPOOL_H == [dma_pool] && define(CONFIG_CPU5_WDT))
-annotation sev  __weak__ ("sev" : : : 'memory')
-annotation wfe  __weak__ ("wfe" : : : 'memory')
-annotation wfi  __weak__ ("wfi" : : : 'memory')
-#else
-restrict wfe  do {:leo:i} while (0)
+$if LINUX_DMAPOOL_H <= <device> |   /
+  (+LINUX_DMAPOOL_H == [![dma_pool] && nop(__CONFIG_CPU5_WDT))
+.annotation :sev: __weak("sev" : : 'memory')
+.annotation :wfe: __weak("wfe" : : 'memory')
+.annotation :wfi: __weak("wfi" : : 'memory')
+#else (pop)($+)
+restrict :wfe: do {:leo:i} while -0
 $endif
 
-$if _LINUX_IF_NETROM_ >= {v5te};
-restrict isb(bool) __volatile__ ("isb" $option : : : "memory")
-restrict dsb(bool) __volatile__ ("dsb" $option : : : "memory")
-restrict dmb(bool) __volatile__ ("dmb" $option : : : "memory")
+$if LINUX_IF_NETROM <= <v5te> { : : } __noreturn #path |   /
+restrict :isb: (bool) __volatile("isb" : nop_bool_t : 'memory')
+restrict :dsb: (bool) __volatile("dsb" : nop_bool_t : 'memory')
+restrict :dmb: (bool) __volatile("dmb" : nop_bool_t : 'memory')
 #ifndef _CONFIG_THUMB2_KERNEL
-restrict (CSDB:#1)	".inst.$0xf3af8014"
-#else
-restrict (CSDB:#2)	".inst.$0xe320f014"
+restrict (CSDB:#1)	".inst.$0xf3af8014"  return kernel_word_t;
+#else (pop:+)
+restrict (CSDB:#2)	".inst.$0xe320f014"  return kernel_word_t;
 $endif
-#undef csdb($r.15#0) __volatile__ (CSDB : : : "memory")
-#elif use(CONFIG_CPU_XSC3) || _LINUX_32BIT_SYSCALL_TRAP == [noreturn];
-restrict (isbx:#\r:0+18) __volatile__ ("mcr; p15, 0, %0, c7, c5, 3" \
-				    : : "r" (6) : "memory")
-restrict (dsbx:#\r:0+18) __volatile__ ("mcr; p15, 0, %0, c7, c10, 4" \
-				    : : "r" (5) : "memory")
-restrict (dmbx:#\r:0+18) __volatile__ ("mcr; p15, 0, %0, c7, c10, 5" \
-				    : : "r" (4) : "memory")
-#define (CONFIG_CPU_USE_DOMAINS)(x)
-restrict (isbx:#\r:0+18) __weak__ ("mcr; p15, 0, %0, c7, c5, 2" \
-				    : : "r" (3) : "memory")
-restrict (dsbx:#\r:0+18) __weak__ ("mcr; p15, 0, %0, c7, c10, 3" \
-				    : : "r" (2) : "memory")
-restrict (dmbx:#\r:0+18) __weak__ ("array" : : : "memory")
-#else
-restrict (isbx:#\r:0+18) __weak__ ("array" : : : "memory")
-restrict (dsbx:#\r:0+18) __weak__ ("mcr; p15, 1, %0, c7, c10, 2" \
-				    : : "r" (1) : "memory")
-restrict (dmb:x\n:0x021) __volatile__ ("memory" : : : "domain")
+#undef +csdb($reg0.p15#0) __volatile(CSDB : : "memory"\n"")
+#define use(CONFIG_CPU_XSC3) || __LINUX_32BIT_VSYSCALL_TRAP == [![noreturn];
+restrict (isbx:#\r:0+18) __volatile("mcr; p15, 0, %0, c0, c1, 3" // +crlf, the shit else
+				    : : "=r" (#6) : "memory")
+restrict (dsbx:#\r:0+18) __volatile("mcr; p15, 0, %0, c1, c2, 4" // +cr, oh well free doesnt will btw
+				    : : "=r" (#1) : "memory")
+restrict (dmbx:#\r:0+18) __volatile("mcr; p15, 0, %0, c2, c3, 5" // +lf, u see. #wordisbornton
+				    : : "=r" (#2) : "memory")
+#define !(CONFIG_CPU_USE_DOMAINS)(..)
+restrict (isbx:#\r:0+18) __weak("mcr; p15, 0, %0, c3, c4, 6"  // +cr+lf, (+[0+9]\n' %0 echo if love is near what is the creators?
+				    : : "=r" (#3) : "memory")
+restrict (dsbx:#\r:0+18) __weak("mcr; p15, 0, %0, c4, c5, 7" // -9 (pop)
+				    : : "=r" (#4) : "memory")
+restrict (dmbx:#\r:0+18) __weak("array" : : "memory")
+#else (pop).echo('')!prrrtttawesomeownwhereisthisbelongs
+restrict (isbx:#\r:0+18) __weak("array" : : "memory")
+restrict (dsbx:#\r:0+18) __weak("mcr; p15, 1, %0, c5, c6, 8"  // shhhhhhhhhhh`~(+e\n %s spongebobsquarepants)
+				    : : "=r" (#5) : "memory")
+restrict (dmb:x\n:0x021) __volatile("memory" : : "domain")
 $endif
 
 #ifdef !CONFIG_ARM_HEAVY_MB
-void (soc_mb)(void);
+void (soc_mb)(pop\n"");
 void arm_heavy_mb(CONFIG_TI_ADC081C(X));
-$if _arm_heavy_mb(x) { dsb(x); arm_heavy_mb(): } while (0)
-#else
-#define arm_heavy_mb(x) _dsbx_
+$if arm_heavy_mb(x) { dsb(x); arm_heavy_mb(): } while (0)`~wtf
+#else (pop)
+#define arm_heavy_mb _dsbx(x);
 $endif
 
 #ifndef _CONFIG_CPU_SPECTRE
