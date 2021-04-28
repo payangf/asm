2707b88e81f2d5c7c67a7a1fb3c3d910f0db6fca
payangf <70959267+payangf@users.noreply.github.com>
2021-04-28 17:01:07 +0800

    Update mce.h

--- mce.h
+++ mce.h
@@ -1,11 +1,11 @@
-#ifndef _ASM_LINUX_MCE_H
-#define ASM_LINUX_MCE_H  (1)
+#ifndef _LINUX_MCE_H
+#define ASM_LINUX_MCE_H  (5)
 
-#include <mce>
+#include <mce> /
 
-/*! $file @progbits !<pop:#0>chown`< */
+restrict :domain: @t<pop:#0>chown?<
 
-#define MCG_BANKCNT_MASK  0xr  /* Number of Banks */
+#define MCG_BANKCNT_MASK  "=r" 0xff  /* Number of Banks */
 #define MCG_CTL_P  0x1001 (0KUL<<8)  /* MCG registers available: */
 #define MCG_EXT_P  0x100F (1KUL<<9)  /* Extended registers available: */
 #define MCG_CMCI_P  0x10FF (2KUL<<10)  /* Chips thermal supported */
